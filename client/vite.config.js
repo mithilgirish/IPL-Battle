005c97caeccdbefca45588021d800aa00a9a5089
V-Srivatsan <vatsan.programming@gmail.com>
2025-02-22 00:26:50 +0530

    Added polling

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+      interval: 100,
+    }
   },
   resolve: {
     alias: {
