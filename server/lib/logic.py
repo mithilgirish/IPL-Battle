083c055d97f0eac44774d9aac797a0cff3c7bfc2
V-Srivatsan <vatsan.programming@gmail.com>
2025-02-26 20:38:02 +0530

    Added reason for elimination

--- server/lib/logic.py
+++ server/lib/logic.py
@@ -2,7 +2,10 @@ from participant.models import *
 from admin.models import Room
 
 def __get_score(team: list[Team]):
-    if not 16 >= len(team) >= 18: return -1
+    invalid = lambda msg: { 'score': -1, 'message': msg }
+
+    if not 16 >= len(team) >= 18:
+        return invalid('Invalid team size')
 
     counts = { 'BA': 0, 'BO': 0, 'AR': 0, 'WK': 0 }
     foreign = score = 0
@@ -12,28 +15,32 @@ def __get_score(team: list[Team]):
         foreign += (not player.domestic)
         score += player.score
 
-    if (
-        foreign > 7 or
-        counts['BA'] < 3 or counts['BO'] < 4 or
-        counts['AR'] < 3 or counts['WK'] < 2
-    ): return -1
 
-    return score
+    if foreign > 7: return invalid('More than 7 foreign players')
+    if counts['BA'] < 3: return invalid('Less than 3 Batsman') 
+    if counts['BO'] < 4: return invalid('Less than 4 Bowlers')
+    if counts['AR'] < 3: return invalid('Less than 3 All-Rounders')
+    if counts['WK'] < 2: return invalid('Less than 2 Wicket Keepers')
+
+    return {
+        'score': score,
+        'message': 'All Good!'
+    }
 
 
 def get_leaderboard(room: Room):
     teams = dict([
         (
             participant.name, 
-            (
-                __get_score(Team.objects.filter(participant=participant)),
-                participant.balance
-            )
+            {
+                **__get_score(Team.objects.filter(participant=participant)),
+                'balance': participant.balance
+            }
         )
         for participant in Participant.objects.filter(room=room)
     ])
 
     return dict([
         (key, teams[key])
-        for key in sorted(teams, key=lambda k: teams[k], reverse=True)
+        for key in sorted(teams, key=lambda k: (teams[k]['score'], teams[k]['balance']), reverse=True)
     ])
